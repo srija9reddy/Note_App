bf8ca8f2c333a184997dd38d1cf1e061a1049239
ShivaSaiKrishna-c0818766 <c0818766@mylambton.ca>
2021-09-23 10:28:25 +0530

    Add_NewNoteVC_class_Added

---
+++ Note_App/Add Notes/AddNoteVc.swift
@@ -0,0 +1,22 @@
+//
+//  AddNoteVc.swift
+//  NoteApp
+//
+//
+
+import UIKit
+
+
+class AddNoteVc: UIViewController {
+
+
+    
+    //MARK:- VIEWDIDLOAD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK:- IBACTIONS
+  
+}
