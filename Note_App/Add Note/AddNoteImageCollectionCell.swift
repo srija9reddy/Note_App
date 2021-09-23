31ae00a1f1f0b1877d8f3940bbb29639d3f1a31d
Navaneeth794 <c0820794@mylambton.ca>
2021-09-23 11:14:57 +0530

    AddNoteImg_CollectionCell_Linking_done

--- Note_App/Add Note/AddNoteImageCollectionCell.swift
+++ Note_App/Add Note/AddNoteImageCollectionCell.swift
@@ -8,4 +8,9 @@ import UIKit
 
 class AddNoteImageCollectionCell: UICollectionViewCell {
     
+    //MARK:- IBOUTLETS
+    @IBOutlet weak var imgBackView: UIView!
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var deleteImgBtn: UIButton!
+    
 }
