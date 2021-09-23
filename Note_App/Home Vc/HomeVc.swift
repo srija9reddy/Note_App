7428a868e601c7f6b0c2eb1d5d951fa22c75562b
srija9reddy <maramsrijareddy@gmail.com>
2021-09-23 09:55:38 +0530

    Home_ViewController_Added

---
+++ Note_App/Home Vc/HomeVc.swift
@@ -0,0 +1,24 @@
+//
+//  HomeVc.swift
+//  NoteApp
+//
+//
+
+import UIKit
+
+class HomeVc: UIViewController {
+
+    //MARK:- IBOUTLETS
+   
+    
+    
+    //MARK:- VIEWDIDLOAD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK:- IBACTIONS
+   
+}
